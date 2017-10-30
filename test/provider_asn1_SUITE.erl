ad08eb88c9b11eca1886060d608f8a5a7b39f903
Martin Hässler <mhssler@gmail.com>
2017-10-30 07:25:28 +0100

    Test umbrella release (with lib/app)

---
+++ test/provider_asn1_SUITE.erl
@@ -0,0 +1,93 @@
+-module(provider_asn1_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+
+-export([all/0,
+         init_per_testcase/2,
+         end_per_testcase/2,
+
+         generates_asn1/1]).
+
+all() ->
+    [generates_asn1].
+
+%%-------------------------------------------------------------------
+%% Tests
+%%-------------------------------------------------------------------
+init_per_testcase(TestCase, Config) ->
+    DataDir = ?config(data_dir, Config),
+    ReleaseDir = filename:join(DataDir, TestCase),
+    {0, _} = os_cmd("rebar3 clean", [{cd, ReleaseDir}]),
+    [{release_dir, ReleaseDir} | Config].
+
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+generates_asn1(Config) ->
+    %% Configure
+    ReleaseDir = ?config(release_dir, Config),
+    ct:pal("~w:~w:~w ==>~n~p~n", [?MODULE, ?LINE, ?FUNCTION_NAME, ReleaseDir]),
+
+    AppEbinDir = filename:join([ReleaseDir, "_build", "default", "lib", app,
+                                "ebin"]),
+
+    TopRebarConfigFile = filename:join(ReleaseDir, "rebar.config"),
+    TopRebarConfigTxt =
+        "{erl_opts, [debug_info]}.\n"
+        "{deps, []}.\n"
+        "{plugins, [{provider_asn1,\n"
+        "   {git, \"https://github.com/mhssler/provider_asn1.git\",\n"
+        "    {tag, \"master\"}}}]}.\n"
+        "{provider_hooks, [{pre, [{compile, {asn, compile}}]},\n"
+        "                  {post, [{clean, {asn, clean}}]}]}.\n"
+        "{asn1_args, [{encoding, per},\n"
+        "             {verbose, true},\n"
+        "             {compile_opts, [der, compact_bit_string]}]}.\n",
+    ok = file:write_file(TopRebarConfigFile, TopRebarConfigTxt),
+
+    %% Generate asn1
+    {0, _} = os_cmd("rebar3 compile", [{cd, ReleaseDir}]),
+    true = code:add_path(AppEbinDir),
+    %% TestAppName = TestAppName:module_info(module),
+
+    %% Verify: ASN.1 module generated and compiled
+    Asn1ModuleName = 'Hello',
+    Asn1ModuleName = Asn1ModuleName:module_info(module),
+    Asn1ModuleAttribs = Asn1ModuleName:module_info(attributes),
+    Asn1Info = proplists:get_value(asn1_info, Asn1ModuleAttribs),
+    ExistingAsn1Options = proplists:get_value(options, Asn1Info),
+    ExpectedAsn1Options = [per, verbose, der, compact_bit_string],
+
+    ct:pal("Expected: ~p", [ExpectedAsn1Options]),
+    ct:pal("Existing: ~p", [ExistingAsn1Options]),
+    [true = lists:member(X, ExistingAsn1Options) || X <- ExpectedAsn1Options],
+
+    ok.
+
+
+%%--------------------------------------------------------------------
+%% Helpers
+%%--------------------------------------------------------------------
+os_cmd(Cmd, ExtraOpts) ->
+    Opts = [stream, in, out, use_stdio, stderr_to_stdout, exit_status] ++
+        ExtraOpts,
+    Port = open_port({spawn, "/bin/sh -s os_cmd 2>&1"}, Opts),
+    port_command(Port, Cmd ++ "; exit $?\n"),
+    {ExitStatus, Output} = receive_until_exit(Port, []),
+    ct:pal("CMD: ~s~n"
+           "OPTS: ~p~n"
+           "EXIT STATUS: ~p~n"
+           "OUTPUT: ~s", [Cmd, Opts, ExitStatus, Output]),
+    {ExitStatus, Output}.
+
+receive_until_exit(Port, Acc) ->
+    receive
+        {Port, {data, Data}} ->
+            receive_until_exit(Port, [Data | Acc]);
+        {Port, {exit_status, ExitStatus}} ->
+            {ExitStatus, lists:flatten(lists:reverse(Acc))}
+    end.
+
+%% FIXME: Use rebar_utils:sh/2 instead
+%% rebar_utils:sh(?FMT("git checkout -q ~ts", [rebar_utils:escape_chars(Rev)]),
+%%                   [{cd, Dir}]).
